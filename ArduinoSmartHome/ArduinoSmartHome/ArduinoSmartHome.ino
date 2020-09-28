e5e621bb179f683f788bc8b58c7fcd7b45d74ce6
will <anin0919@gmail.com>
2020-09-28 16:02:33 +0800

    依狀態不同送出 alert, normal 訊息給控制器

--- ArduinoSmartHome/ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome/ArduinoSmartHome.ino
@@ -1,23 +1,29 @@
-void setup() {
- pinMode(12, OUTPUT);
- pinMode(11, OUTPUT);
- pinMode(7, OUTPUT);
-}
+void setup()
 
-void loop() {
+{
+pinMode(12, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(2, INPUT);
+Serial.begin(9600);
+}
+void loop()
+{
+if(digitalRead(2)==HIGH) {
+Serial.println("HIGH");
 digitalWrite(12, HIGH);
 digitalWrite(11, LOW);
 tone(7, 1000, 500);
-tone(7, 900, 500);
-tone(7, 800, 500);
-tone(7, 700, 500);
-tone(7, 600, 500);
 delay(500);
 digitalWrite(12, LOW);
 digitalWrite(11, HIGH);
-tone(7, 500, 500);
-tone(7, 600, 500);
-tone(7, 700, 500);
 tone(7, 800, 500);
 delay(500);
+}
+else {
+Serial.println("LOW");
+digitalWrite(11, LOW);
+digitalWrite(12, LOW);
+}
+
 }
